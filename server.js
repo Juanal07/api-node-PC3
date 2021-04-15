96d5d1a4127dfb6b9b4e1b2f0617078ef5e59681
Juan Alberto Raya <jjrr1307@gmail.com>
2021-04-15 12:10:01 +0200

    cors

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 // const bodyParser = require("body-parser");
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
-// var corsOptions = {
-//     origin: "http://localhost:8080"
-// };
-// app.use(cors(corsOptions));
+var corsOptions = {
+    origin: "http://localhost:8080"
+};
+app.use(cors(corsOptions));
 
 app.set("port", process.env.PORT || 8080);
 
