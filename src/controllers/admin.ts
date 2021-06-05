658842fed04443d55eb44126545525920c77e825
Juan Alberto Raya <jjrr1307@gmail.com>
2021-06-05 18:41:39 +0200

    auto push

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -6,7 +6,14 @@ async function getUsers(req: any, res: any) {
         const sqlQuery =
             "SELECT idUser, active, name, email, dateSignIn FROM user where admin=0";
         const result = await pool.query(sqlQuery);
-        console.log(result);
+        for (let i = 0; i < result.length; i++) {
+            result[i].dateSignIn =
+                result[i].dateSignIn.getDate() +
+                "-" +
+                (result[i].dateSignIn.getMonth() + 1) +
+                "-" +
+                result[i].dateSignIn.getFullYear();
+        }
         res.status(200).json({
             status: 200,
             data: result,
@@ -20,7 +27,7 @@ async function getUsers(req: any, res: any) {
 async function user(req: any, res: any) {
     try {
         // const { idUser } = req.body;
-        
+
         const sqlQuery = "SELECT name, email, dateSignIn FROM user";
         const result = await pool.query(sqlQuery);
         const name = result[0]["name"];
@@ -34,26 +41,23 @@ async function user(req: any, res: any) {
                 dateSignIn,
             },
         });
-        
     } catch (err) {
         console.log(err);
         res.status(400).send(err);
     }
 }
 
-
 async function deleteUser(req: any, res: any) {
     try {
         const { idUsuario } = req.body;
-        
+
         const sqlQuery = "UPDATE user SET noDeleted = 0 WHERE idUser = ?";
         const result = await pool.query(sqlQuery, [idUsuario]);
-        console.log(result)
-        
-        res.status(200).json({ 
-            status:200, 
-            
-        });   
+        console.log(result);
+
+        res.status(200).json({
+            status: 200,
+        });
     } catch (err) {
         console.log(err);
         res.status(400).send(err);
@@ -61,7 +65,7 @@ async function deleteUser(req: any, res: any) {
 }
 
 //Poblaciones Escrapeadas
-async function muniScrapeados (req: any, res: any) {
+async function muniScrapeados(req: any, res: any) {
     try {
         const sqlQuery =
             "SELECT (particular*100/total) porcentaje FROM (SELECT COUNT(IdMunicipality) total FROM municipality) QUERY_a LEFT JOIN (SELECT COUNT(IdMunicipality) particular FROM search WHERE expDate>NOW())query_b ON (1=1)";
@@ -78,7 +82,7 @@ async function muniScrapeados (req: any, res: any) {
 }
 
 //Usuarios registrados por mes
-async function RegisterMes (req: any, res: any) {
+async function RegisterMes(req: any, res: any) {
     try {
         const sqlQuery =
             "SELECT count(NAME) AS Usuarios, month(dateSignIn) AS Mes FROM user where admin=0 GROUP BY month(dateSignIn)";
@@ -95,7 +99,7 @@ async function RegisterMes (req: any, res: any) {
 }
 
 //Cuentas en porcentaje de usuarios activadas y desactivadas
-async function activosInactivos (req: any, res: any) {
+async function activosInactivos(req: any, res: any) {
     try {
         const sqlQuery =
             "SELECT (activos*100/total) activos,  (100 - (activos*100/total)) inactivos FROM ((SELECT COUNT(idUser) total FROM user) AS querytotal LEFT JOIN (SELECT COUNT( DISTINCT (idUser))activos FROM log WHERE logout is NULL) AS querylog ON (1=1))";
@@ -111,8 +115,8 @@ async function activosInactivos (req: any, res: any) {
     }
 }
 
-//rankings de usuarios activos y tiempo total de cada uno 
-async function rankingsActivos (req: any, res: any) {
+//rankings de usuarios activos y tiempo total de cada uno
+async function rankingsActivos(req: any, res: any) {
     try {
         const sqlQuery =
             "SELECT user.name, SEC_TO_TIME(SUM(timediff(logout, login))) AS activos " +
@@ -130,7 +134,7 @@ async function rankingsActivos (req: any, res: any) {
 }
 
 //media de horas por dia
-async function actividadDiaria (req: any, res: any) {
+async function actividadDiaria(req: any, res: any) {
     try {
         const sqlQuery =
             "(SELECT date(logout) AS fecha,sec_to_time(sum(timeDIFF(logout, login)))as actividadTotal, count(distinct(idUser))as usuariosConectados, sec_to_time(sum(timeDIFF(logout, login)) /COUNT(distinct(idUser))) AS media " +
@@ -147,35 +151,34 @@ async function actividadDiaria (req: any, res: any) {
     }
 }
 //Municipios mas buscados
-async function muniBuscados (req: any, res: any) {
+async function muniBuscados(req: any, res: any) {
     // const {idUser} = req.body;
     // console.log("idUser "+idUser);
     // if(idUser!=43){
     //     console.log("Error");
     //     res.status(400).send();
-        
+
     // }else{
-        try {
-            const sqlQuery =
-                "SELECT municipality.name, COUNT(search.idMunicipality) AS numBusquedas " +
-                "FROM search JOIN municipality ON search.idMunicipality = municipality.idMunicipality GROUP BY search.idMunicipality ORDER BY numBusquedas DESC";
-            const result = await pool.query(sqlQuery);
-            // console.log(result);
-            res.status(200).json({
-                status: 200,
-                data: result,
-            });
-        } catch (err) {
-            console.log("Error al obtener los usuarios", err);
-            res.status(400).send(err);
-        }
+    try {
+        const sqlQuery =
+            "SELECT municipality.name, COUNT(search.idMunicipality) AS numBusquedas " +
+            "FROM search JOIN municipality ON search.idMunicipality = municipality.idMunicipality GROUP BY search.idMunicipality ORDER BY numBusquedas DESC";
+        const result = await pool.query(sqlQuery);
+        // console.log(result);
+        res.status(200).json({
+            status: 200,
+            data: result,
+        });
+    } catch (err) {
+        console.log("Error al obtener los usuarios", err);
+        res.status(400).send(err);
+    }
     // }
 }
 
 async function actData(req: any, res: any) {
     try {
-        const subprocessNoticias = spawn("python", [
-            "scrapers/actData.py"]);
+        const subprocessNoticias = spawn("python", ["scrapers/actData.py"]);
         res.status(200);
     } catch (err) {
         console.log("Error en la ejecución de la actualización", err);
@@ -194,5 +197,4 @@ export default {
     actividadDiaria,
     muniBuscados,
     actData,
-
 };
