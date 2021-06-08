b74366948eadf4c6e4419c89fca577bcd27ee136
Juan Alberto Raya <jjrr1307@gmail.com>
2021-06-08 22:04:00 +0200

    auto push

--- src/keys.ts
+++ src/keys.ts
@@ -1,20 +1,20 @@
-module.exports = {
-    database: {
-        host: "2.139.176.212",
-        user: "pr_softlusion",
-        password: "Softlusion",
-        database: "prsoftlusion",
-        port: "3306",
-        connectionLimit: 1,
-    },
-};
-
-//Keys para trabajar en BBDD local con XAMPP
 // module.exports = {
 //     database: {
-//         host: "localhost",
-//         user: "root",
-//         password: "",
+//         host: "2.139.176.212",
+//         user: "pr_softlusion",
+//         password: "Softlusion",
 //         database: "prsoftlusion",
+//         port: "3306",
+//         connectionLimit: 1,
 //     },
 // };
+
+// Keys para trabajar en BBDD local con XAMPP
+module.exports = {
+    database: {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "prsoftlusion",
+    },
+};
